5f5b52968b683d7a9ae88b5c343fbd4b686c7b68
Russell Gordon <rgordon@rsgc.on.ca>
2016-12-18 18:52:03 -0500

    Added art assets and now draw a brick wall in middle of screen

--- CollisionTest/GameScene.swift
+++ CollisionTest/GameScene.swift
@@ -10,10 +10,28 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-        
+    
+    // MARK: Properties
+    let tileCount : CGFloat = 8.0
+    var tileSize : Int = 0
+    
     override func didMove(to view: SKView) {
         
+        // Set background
         backgroundColor = SKColor.black
+        
+        // Determine tile size
+        tileSize = Int(self.size.height / tileCount)
+        
+        // Draw a brick wall in the middle of the screen
+        for i in 1...Int(tileCount) {
+            let wall = SKSpriteNode(imageNamed: "brick")
+            wall.position = CGPoint(x: self.size.width / 2, y: CGFloat(tileSize / 2 + tileSize * i) )
+            wall.zPosition = 1
+            wall.setScale(6.0)
+            addChild(wall)
+        }
+        
 
     }
     
