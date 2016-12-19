a7e78a20ebb9a3b7e356115843b46227c29b67a6
Russell Gordon <rgordon@rsgc.on.ca>
2016-12-18 19:04:53 -0500

    Player now moves following taps on screen.

--- CollisionTest/GameScene.swift
+++ CollisionTest/GameScene.swift
@@ -15,7 +15,8 @@ class GameScene: SKScene {
     let tileCount : CGFloat = 8.0
     var tileSize : Int = 0
     var player = SKSpriteNode()
-
+    var playerSpeed : CGFloat = 1000
+    
     override func didMove(to view: SKView) {
         
         // Set background
@@ -42,6 +43,36 @@ class GameScene: SKScene {
 
     }
     
+    // This responds to a single touch
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // A touch actually has many points (since a finger is large) so
+        // only proceed if we can get the first touch
+        guard let touch = touches.first else {
+            return
+        }
+        
+        // Get the location of the first touch
+        let touchLocation = touch.location(in: self)
+        
+        // Get the distance between player's current position and the touch location
+        let distanceToTouch = distance(from: touchLocation, to: player.position)
+        
+        // Get the time player should take to arrive at this destination
+        let time = TimeInterval(distanceToTouch / playerSpeed)
+        
+        // Create the move action
+        let actionMove = SKAction.move(to: touchLocation, duration: time)
+        
+        // Run the move action
+        player.run(actionMove, withKey: "playerMoving")
+    }
+    
+    // This determines the distance between two points using the Pythagorean Theorem
+    func distance(from : CGPoint, to: CGPoint) -> CGFloat {
+        
+        return sqrt(pow(from.x - to.x, 2) + pow(from.y - to.y, 2))
+    }
     
     
 }
